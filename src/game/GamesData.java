47ae0ab724af1909314b6cdf06f60e0410022e67
Hronol <Hronol123@gmail.com>
2020-01-08 19:39:51 +0100

    first commit

---
+++ src/game/GamesData.java
@@ -0,0 +1,17 @@
+package game;
+
+public class GamesData {
+    private String gameTitle;
+    private String gamePlatform;
+    private int gameYear;
+    private String gameCategory;
+
+    public GamesData(String gameTitle, String gamePlatform, int gameYear, String gameCategory) {
+        this.gameTitle = gameTitle;
+        this.gamePlatform = gamePlatform;
+        this.gameYear = gameYear;
+        this.gameCategory = gameCategory;
+    }
+}
+
+
